1decaacc949636f1413d15e28003f3e2181ad0fb
Sarah Strawn <sarah.c.strawn@gmail.com>
2014-08-19 18:14:49 -0700

    Obtainied hype document names

--- zyChromeExtension.js
+++ zyChromeExtension.js
@@ -6,17 +6,31 @@ function hashChanged() {
     var hashParts = pageHash.split('/');
 
     if (hashParts[3] === 'chapter') {
-        console.log("hello from zyChromeExtension and we'll just test it");
-        displayHypeDocumentNames():
-        displayContentResourceIds();
+        window.setTimeout(function() {
+            displayHypeDocumentNames();
+            displayContentResourceIds();
+        }, 1000);
     }
 }
 
 function displayHypeDocumentNames() {
-
+    var hypeAnimationDivs = toArray(document.querySelectorAll('.hypeAnimation > div'));
+    hypeAnimationDivs.forEach(function(element) {
+        var documentName = element.getAttribute('hype_documentname');  
+        console.log(documentName);
+    });
 }
 
 function displayContentResourceIds() {
 
 }
 
+function toArray(nodeList) {
+    var array = [];
+
+    for (var i = 0; i < nodeList.length; ++i) {
+        array.push(nodeList[i]);
+    }
+
+    return array;
+}
