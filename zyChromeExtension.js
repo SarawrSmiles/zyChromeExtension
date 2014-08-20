94e61489eb08a8fc9645d527c978d82f83af34a2
Sarah Strawn <sarah.c.strawn@gmail.com>
2014-08-19 17:58:05 -0700

    Stubbed functions for getting Hype document names and CR ids

--- zyChromeExtension.js
+++ zyChromeExtension.js
@@ -1,1 +1,22 @@
-console.log("hello from zyChromeExtension and we'll just test it");
+
+window.onhashchange = hashChanged;
+
+function hashChanged() {
+    var pageHash = window.location.hash;
+    var hashParts = pageHash.split('/');
+
+    if (hashParts[3] === 'chapter') {
+        console.log("hello from zyChromeExtension and we'll just test it");
+        displayHypeDocumentNames():
+        displayContentResourceIds();
+    }
+}
+
+function displayHypeDocumentNames() {
+
+}
+
+function displayContentResourceIds() {
+
+}
+
